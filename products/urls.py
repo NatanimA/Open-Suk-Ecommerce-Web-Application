a26924872c2e27d084b819fc98e0056be953b617
Nathan <NatanimAbesha@gmail.com>
2022-07-13 14:18:46 +0300

    Created City-Slug to navigate to local adds

--- products/urls.py
+++ products/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('cities/<slug:city_slug>',views.productList,name='product_list_city'),
     path('products/<slug:slug>/',views.ProductDetailView.as_view(),name='product_detail'),
     path('post/new/',views.ProductCreate.as_view(),name='post'),
-    path('categories/<slug:category_slug>', views.productList, name='product_list_category'),
+    path('categories/<slug:category_slug>',
+         views.productList, name='product_list_category'),
     # path('',views.ProductDelete.as_view(),name='product_delete'),
 ]
 
