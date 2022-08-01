397df4053714c2e392752f4a4ac706c9d8054968
natanimabesha <natanimabesha@www.zhewutech.com>
2022-08-01 21:00:57 +0300

    Added extra views to the venv

--- opensuk/settings.py
+++ opensuk/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'extra_views',
     'hitcount',
     'phonenumber_field',
     'products',
